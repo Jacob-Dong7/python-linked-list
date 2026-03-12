cb0152cc6dfad6b1df024f67cd1f725e1ed78a5c
Jowan <s4172023@student.rmit.edu.au>
2026-03-13 01:03:44 +1100

    feat: got some done

--- LinkedList.py
+++ LinkedList.py
@@ -3,7 +3,9 @@ class linked_list:
     def __init__(self):
         self.head = None
         self.length = 0
-        
+    
+    def get_length(self):
+        return self.length
     
     def add_front(self, value):
         new_node = Node(value)
@@ -13,6 +15,8 @@ class linked_list:
             self.head = new_node
         else:
             self.set_new(new_node)
+        
+        self.length += 1
 
     def add_tail(self, value):
         new_node = Node(value)
@@ -21,6 +25,8 @@ class linked_list:
             self.head = new_node
         else:
             self.find_tail(new_node)
+
+        self.length += 1
 
     def find_tail(self, new_node):
         current = self.head
@@ -31,13 +37,45 @@ class linked_list:
                 current.set_next(new_node)
                 new_node.set_next(None)
                 return
-                
+            
+    def add_at(self, value, index):
+        new_index = int(index)
+        new_node = Node(value)
+
+        if new_index == 1:
+            self.set_new(new_node)
+            return
+        
+        if new_index > self.length or new_index <= 0:
+            print("Index out of bound")
+            return
+        else:
+            i = 1
+            current = self.head
+            previous = current
+            while True:
+
+                if i == new_index:
+                    previous.set_next(new_node)
+                    new_node.set_next(current)
+                    self.length += 1
+                    return
+                else:
+                    previous = current
+                    current = current.get_next()
+                    i += 1  
+                    continue            
+        
+    
     def set_new(self, new_node):
         new_node.set_next(self.head)
         self.head = new_node
 
     def print(self):
         current = self.head
+        if self.length <= 0:
+            print("Empty")
+            return
         while True:
             if current.get_next() != None:
                 print(current.get_value(),end=" -> ")
--- Main.py
+++ Main.py
@@ -1,8 +1,32 @@
 from LinkedList import linked_list
-print("Linked List")
 linkedlist = linked_list()
-linkedlist.add_front("100")
-linkedlist.add_front("100000")
-linkedlist.add_front("1012")
-linkedlist.add_tail("2020")
-linkedlist.print()
+
+print("Linked List")
+while True:
+    print("[1] Add front")
+    print("[2] Add Tail")
+    print("[3] Add at index")
+    print("[4] Print")
+
+    user_input = input("Select Choice: ")
+
+    if user_input == "1":
+        value = input("Select value: ")
+        print()
+        linkedlist.add_front(value)
+    elif user_input == "2":
+        value = input("Select value: ")
+        print()
+        linkedlist.add_tail(value)
+    elif user_input == "3":
+        value = input("Select value: ")
+        print()
+        print("Length: ",end="") 
+        print(linkedlist.get_length())
+        index = input("Select index: ")
+        print()
+        linkedlist.add_at(value, index)
+    elif user_input == "4":
+        linkedlist.print()
+    elif user_input == "-1":
+        quit
